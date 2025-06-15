ddf89ca336887f90511b070176fb4c82baa26b50
Nguyen Minh Hieu <minhhieuit.info@gmail.com>
2025-06-15 10:24:43 +0700

    update

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,19 +1,23 @@
-type PageProps = {
+import { Metadata } from 'next';
+
+type Props = {
   params: {
     slug: string;
   };
 };
 
-export async function generateMetadata({ params }: PageProps) {
-  return { title: `Post: ${params.slug}` };
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  return {
+    title: `Post: ${params.slug}`,
+  };
 }
 
-export default function Page({ params }: PageProps) {
+export default function Page({ params }: Props) {
   return (
     <>
       <h1>Slug: {params.slug}</h1>
-      <p>TrainingGitHubActions</p>
-      <p>Welcome to GithubAction</p>
+      <p>Training GitHub Actions</p>
+      <p>Welcome to GitHub Action</p>
     </>
   );
 }
