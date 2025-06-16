eb6efe9e1825b8ec3cedd12811772ae8d49fd6c7
Nguyen Minh Hieu <minhhieuit.info@gmail.com>
2025-06-16 07:09:58 +0700

    update

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,14 +1,24 @@
+import { Metadata } from 'next';
+
 type Params = {
   slug: string;
 };
 
-export async function generateMetadata({ params }: { params: Params }) {
+export async function generateMetadata({
+  params,
+}: {
+  params: Params;
+}): Promise<Metadata> {
   return {
     title: `Post: ${params.slug}`,
   };
 }
 
-export default function Page({ params }: { params: Params }) {
+export default function Page({
+  params,
+}: {
+  params: Params;
+}) {
   return (
     <>
       <h1>Slug: {params.slug}</h1>
